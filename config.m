b697e35bcacdb2c441f0e97e38569e375cc6bd06
Rahul Moghe <rahul.moghe@utexas.edu>
2016-11-23 15:13:32 -0600

    config file

---
+++ config.m
@@ -0,0 +1,11 @@
+% This file reads the system parameters for the pendubot system as global variables
+
+global 			m
+global 			m
+global 			m
+global 			m
+global 			m
+global 			m
+global 			m
+
+m = 1;
